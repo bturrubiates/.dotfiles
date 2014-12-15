34b4e3be61e4dda2a9d4fb1f7319e2924c34d81b
Ben Turrubiates <ben@turrubiat.es>
2014-12-14 23:49:20 -0600

    Add tree to installs list for brew on OS X.

--- bin/brewfile.sh
+++ bin/brewfile.sh
@@ -4,7 +4,7 @@ brew tap homebrew/versions
 brew tap staticfloat/julia
 
 formulas=(zsh-syntax-highlighting leiningen elixir ghc cabal-install python3 \
-          clojurescript rust julia go tmux ssh-copy-id rcm)
+          clojurescript rust julia go tmux ssh-copy-id rcm tree)
 
 for formula in "${formulas[@]}"
 do
